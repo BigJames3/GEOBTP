1efb61fb355bd530953aae0628315cded44aedcf
BigJames3 <traorejames34@gmail.com>
2023-12-08 17:55:09 +0100

    Test

--- accueil_page_des_certificats.php
+++ accueil_page_des_certificats.php
@@ -4,7 +4,7 @@ session_start();
 // Vérifier si l'utilisateur est connecté
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     header("location: account.php"); // Redirection vers la page de connexion si l'utilisateur n'est pas connecté
-    exit;
+    exit; 
 }
 // Le reste du contenu de la page formulaire.php ici
 ?>
